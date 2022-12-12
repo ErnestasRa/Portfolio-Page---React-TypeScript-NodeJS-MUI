e35da47dca6c096c1f10a4fd507de08f2c1a8945
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2022-12-12 12:47:39 +0200

    feat: Eslint Airbnb style configured

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,7 +1,16 @@
 import * as React from "react";
+import {
+  Container,
+  Box,
+  Paper,
+} from '@mui/material'
+import ResponsiveAppBar from "../components/form-controls/app-bar";
 
 const MainPage: React.FC = () => {
-  return <div>MainPage</div>;
+  return <Container>
+    <ResponsiveAppBar />
+    <Box>Hi</Box>
+  </Container>
 };
 
 export default MainPage;
