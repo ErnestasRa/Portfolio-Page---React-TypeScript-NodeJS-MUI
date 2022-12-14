e561ced6198768865b496f55a12cab69689d660c
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2022-12-14 19:28:45 +0200

    feat: profile pic implemented

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -4,12 +4,38 @@ import {
   Box,
 } from '@mui/material';
 import ResponsiveAppBar from '../components/form-controls/app-bar';
+import { get } from '../functions/http';
+import ProfilePic from '../images/profile-pic.jpg';
 
-const MainPage: React.FC = () => (
+const MainPage: React.FC = () => {
+  const getProfilePicture = async () => {
+    const res = await get('profilepic');
+    console.log(res);
+  };
+
+  React.useEffect(() => {
+    getProfilePicture();
+  }, []);
+
+return (
   <Container>
     <ResponsiveAppBar />
-    <Box>hi</Box>
+    <Box
+      alt="my profile image"
+      component="img"
+      src={ProfilePic}
+      sx={{
+      objectFit: 'contain',
+      height: 300,
+      width: 300,
+      borderRadius: 100,
+      maxHeight: { xs: 233, md: 167 },
+      maxWidth: { xs: 350, md: 250 },
+    }}
+
+    />
+
   </Container>
 );
-
+};
 export default MainPage;
