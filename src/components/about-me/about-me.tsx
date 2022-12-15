31f109e00488dbbcaaa451e3133535b1e6ecdff8
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2022-12-15 14:33:57 +0200

    feat: added hrefs to my social media pages

--- src/components/about-me/about-me.tsx
+++ src/components/about-me/about-me.tsx
@@ -3,6 +3,7 @@ import {
     Box,
     Typography,
 } from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import MediaIcons from './media-icons';
 
 const AboutMe: React.FC = () => (
@@ -19,7 +20,12 @@ const AboutMe: React.FC = () => (
       one of my biggest hobbies, since I was little - programming.
     </Typography>
     <Typography sx={{ textAlign: { md: 'inherit', xs: 'center' }, fontFamily: 'cursive' }}>
-      I`am React with TypeScript Full-stack Developer.
+      I`am React with TypeScript MERN stack Developer.
+    </Typography>
+    <Typography sx={{ textAlign: { md: 'inherit', xs: 'center' }, fontFamily: 'cursive', mt: '2vh' }}>
+      Contact me here
+      {' '}
+      <KeyboardArrowDownIcon sx={{ mt: '1vh' }} />
     </Typography>
     <MediaIcons />
   </Box>
--- src/components/about-me/media-icons.tsx
+++ src/components/about-me/media-icons.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {
     Box,
+    IconButton,
 } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
@@ -11,11 +12,18 @@ const MediaIcons: React.FC = () => (
     display: 'flex',
     flexDirection: 'row',
     justifyContent: { xs: 'center', md: 'flex-start' },
+    mt: '2vh',
     }}
   >
-    <LinkedInIcon sx={{ fontSize: '2.5rem' }} />
-    <FacebookIcon sx={{ fontSize: '2.5rem' }} />
-    <GitHubIcon sx={{ fontSize: '2.5rem' }} />
+    <IconButton href="https://www.linkedin.com/in/anarkii/">
+      <LinkedInIcon sx={{ fontSize: '2.5rem' }} />
+    </IconButton>
+    <IconButton href="https://www.facebook.com/R.Ernis">
+      <FacebookIcon sx={{ fontSize: '2.5rem' }} />
+    </IconButton>
+    <IconButton href="https://github.com/ErnestasRa">
+      <GitHubIcon sx={{ fontSize: '2.5rem' }} />
+    </IconButton>
   </Box>
   );
 
