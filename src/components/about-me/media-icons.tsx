ae554e35e980a30edac487220cd6460ded86cd06
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2022-12-23 00:42:13 +0200

    feat: added GameWithMe Section

--- src/components/about-me/media-icons.tsx
+++ src/components/about-me/media-icons.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {
     Box,
     IconButton,
-
 } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
