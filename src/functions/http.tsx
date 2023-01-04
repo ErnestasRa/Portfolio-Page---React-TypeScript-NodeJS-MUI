ab8c65c6ff9fcc52af92975b737da210239e9113
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2023-01-04 12:53:13 +0200

    chore: console.log removed

--- src/functions/http.tsx
+++ src/functions/http.tsx
@@ -1,7 +1,5 @@
 const SERVER_IP = process.env.REACT_APP_SERVER_API_ADDRESS;
 
-console.log(SERVER_IP);
-
 const post = async (url:string, data: any) => {
     const options: RequestInit = {
       method: 'POST',
