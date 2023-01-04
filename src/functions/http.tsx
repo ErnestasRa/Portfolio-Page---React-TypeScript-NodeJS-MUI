e6bc5befd3b1cd9abe96d4674b98dcd48e5105fa
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2023-01-04 12:52:52 +0200

    chore: dotenv fixes

--- src/functions/http.tsx
+++ src/functions/http.tsx
@@ -1,5 +1,7 @@
 const SERVER_IP = process.env.REACT_APP_SERVER_API_ADDRESS;
 
+console.log(SERVER_IP);
+
 const post = async (url:string, data: any) => {
     const options: RequestInit = {
       method: 'POST',
